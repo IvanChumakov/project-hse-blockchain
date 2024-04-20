115cb766a6c4f3cc16b9da889f3280861f2c4eb1
Ivan Chumakov <iochumakov@edu.hse.ru>
2024-04-20 14:40:59 +0300

    changes in front

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -2,10 +2,17 @@ function myFunction() {
     alert("Hello, world!");
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button_to_click');
+    button.addEventListener('click', () => {
+        alert('Hello World');
+    });
+});
+
 document.getElementById('addressForm').addEventListener('submit', (event) => {
     event.preventDefault();
 
-    let data = document.getElementById("address")
+    let data = document.getElementById("address") 
     if (data.value == "") {
         alert("You should input something")
     } else {
