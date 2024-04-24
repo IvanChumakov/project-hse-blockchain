83b845fdbef23840643426dd3e35ea31a9ad760a
Сидоров Александр Михайлович <amsidorov_2@edu.hse.ru>
2024-04-24 09:32:18 +0300

    updated for deployment

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,7 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-ignition-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-verify");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { metamask_private_key, etherscanAPIKey } = require('./secrets.json');
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.19",
+
+  networks: {
+    sepolia: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      accounts: [metamask_private_key],
+    },
+  },
+  etherscan: {
+    apiKey: etherscanAPIKey,
+  },
 };
